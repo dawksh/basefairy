31a18b535d72344eabbbd1e05f1198e13c00902f
daksh <akshd@outlook.com>
2024-09-21 22:38:59 +0530

    feat: add basic ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,63 +1,66 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
-    darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-  	extend: {
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
-};
-export default config;
+const config = {
+	darkMode: ["class"],
+	content: [
+		'./pages/**/*.{ts,tsx}',
+		'./components/**/*.{ts,tsx}',
+		'./app/**/*.{ts,tsx}',
+		'./src/**/*.{ts,tsx}',
+	],
+	prefix: "",
+	theme: {
+		extend: {
+			colors: {
+				main: '#88aaee',
+				mainAccent: '#4d80e6',
+				overlay: 'rgba(0,0,0,0.8)',
+
+				// light mode
+				bg: '#dfe5f2',
+				text: '#000',
+				border: '#000',
+
+				// dark mode
+				darkBg: '#272933',
+				darkText: '#eeefe9',
+				darkBorder: '#000',
+				secondaryBlack: '#1b1b1b',
+			},
+			borderRadius: {
+				base: '5px'
+			},
+			boxShadow: {
+				light: '3px 4px 0px 0px #000',
+				dark: '3px 4px 0px 0px #000',
+			},
+			translate: {
+				boxShadowX: '3px',
+				boxShadowY: '4px',
+				reverseBoxShadowX: '-3px',
+				reverseBoxShadowY: '-4px',
+			},
+			fontWeight: {
+				base: '500',
+				heading: '700',
+			},
+			animation: {
+				marquee: "marquee 10s linear infinite",
+				marquee2: "marquee2 10s linear infinite",
+			},
+			keyframes: {
+				marquee: {
+					"0%": { transform: "translateX(0%)" },
+					"100%": { transform: "translateX(-100%)" },
+				},
+				marquee2: {
+					"0%": { transform: "translateX(100%)" },
+					"100%": { transform: "translateX(0%)" },
+				},
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
