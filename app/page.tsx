5d69e86e91231aad1637b1ee2d4838084ee6d33b
daksh <akshd@outlook.com>
2024-09-22 02:08:21 +0530

    feat: add info

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,10 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Marquee items={["🪄 basefairy", "🪄 basefairy", "🪄 basefairy", "🪄 basefairy", "🪄 basefairy"]} />
-      <Input value={name} onChange={(e) => setName(e.target.value)} placeholder="enter basename" className="w-2/5 h-16" />
+      <div className="w-2/5 h-16">
+        <Input value={name} onChange={(e) => setName(e.target.value)} placeholder="enter basename" className="h-full" />
+        <p className="text-sm italic my-2">start typing to check basename availability</p>
+      </div>
       {Boolean(availability) && <SendDialog name={name} />}
     </div>
   );
