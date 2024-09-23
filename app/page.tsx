6c06d25008b66f8b4a9f662076c69682dcdda104
daksh <akshd@outlook.com>
2024-09-23 21:15:29 +0530

    feat: add focus on mount

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 import Marquee from "@/components/ui/marquee";
 import { toast } from "@/hooks/use-toast";
 import { baseRegistrar } from "@/lib/abis";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useReadContract } from "wagmi";
 import { useDebounce } from 'use-debounce';
 import SendDialog from "@/components/shared/send";
@@ -12,6 +12,7 @@ import SendDialog from "@/components/shared/send";
 export default function Home() {
   const [name, setName] = useState<string>("")
   const [debouncedName] = useDebounce(name, 1000)
+  const addInputRef = useRef<HTMLInputElement | null>(null);
 
   const { data: availability } = useReadContract({
     address: baseRegistrar.address as `0x${string}`,
@@ -23,6 +24,10 @@ export default function Home() {
     },
     scopeKey: "availability"
   })
+
+  useEffect(() => {
+    addInputRef.current?.focus()
+  }, [])
 
   useEffect(() => {
     if (debouncedName != "") {
@@ -46,7 +51,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-items-center min-h-screen pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Marquee items={["🪄 basefairy", "🪄 basefairy", "🪄 basefairy", "🪄 basefairy", "🪄 basefairy"]} />
       <div className="w-2/5 h-16">
-        <Input value={name} onChange={(e) => setName(e.target.value)} placeholder="enter basename (without .base.eth)" className="h-full" />
+        <Input ref={addInputRef} value={name} onChange={(e) => setName(e.target.value)} placeholder="enter basename (without .base.eth)" className="h-full" />
         <p className="text-sm italic my-2">start typing to check basename availability</p>
       </div>
       {Boolean(availability) && <SendDialog name={name} />}
